861e1e3a74ee84bb33e416d93e309ced3b944d19
cygon23 <rweyemamu088@gmail.com>
2025-09-21 06:47:45 +0300

    #removing auth page from havig navigation and footer
    Signed-off-by: cygon23 <rweyemamu088@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,11 @@ import EditBlog from "./pages/EditBlog";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Tools from "./pages/Tools";
+import ResumeChecker from "./pages/tools/ResumeChecker";
+import SalaryCalculator from "./pages/tools/SalaryCalculator";
+import SkillsAssessment from "./pages/tools/SkillsAssessment";
+import CareerPath from "./pages/tools/CareerPath";
+import InterviewPrep from "./pages/tools/InterviewPrep";
 import Forum from "./pages/Forum";
 import Events from "./pages/Events";
 import Auth from "./pages/Auth";
@@ -30,31 +35,41 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <div className="flex flex-col min-h-screen">
-            <Navigation />
-            <main className="flex-1">
-              <Routes>
-                <Route path="/" element={<Index />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:id" element={<BlogPost />} />
-              <Route path="/create-blog" element={<CreateBlog />} />
-              <Route path="/edit-blog/:id" element={<EditBlog />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/tools" element={<Tools />} />
-                <Route path="/forum" element={<Forum />} />
-                <Route path="/events" element={<Events />} />
-                <Route path="/auth" element={<Auth />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/settings" element={<Settings />} />
-                {/* Placeholder routes for now */}
-                <Route path="/categories" element={<Blog />} />
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
+          <Routes>
+            {/* Auth page without navigation and footer */}
+            <Route path="/auth" element={<Auth />} />
+            
+            {/* All other pages with navigation and footer */}
+            <Route path="*" element={
+              <div className="flex flex-col min-h-screen">
+                <Navigation />
+                <main className="flex-1">
+                  <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/blog/:id" element={<BlogPost />} />
+                    <Route path="/create-blog" element={<CreateBlog />} />
+                    <Route path="/edit-blog/:id" element={<EditBlog />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/tools" element={<Tools />} />
+                    <Route path="/tools/resume-checker" element={<ResumeChecker />} />
+                    <Route path="/tools/salary-calculator" element={<SalaryCalculator />} />
+                    <Route path="/tools/skills-assessment" element={<SkillsAssessment />} />
+                    <Route path="/tools/career-path" element={<CareerPath />} />
+                    <Route path="/tools/interview-prep" element={<InterviewPrep />} />
+                    <Route path="/forum" element={<Forum />} />
+                    <Route path="/events" element={<Events />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/categories" element={<Blog />} />
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </main>
+                <Footer />
+              </div>
+            } />
+          </Routes>
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
